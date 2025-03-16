fcbf6a7a437abb366ed25f8d12cfbe7a393ada84
heidless <lockhart.r@gmail.com>
2025-03-16 04:41:29 +0000

    initiate friends page & link

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "welcome#index"
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'my_friends', to: "users#my_friends"
   get 'search_stock', to: "stocks#search"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
