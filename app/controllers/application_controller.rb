d659311fbeb08d7499b721b84e5ca907ccc0f7b6
heidless <lockhart.r@gmail.com>
2025-03-24 11:27:39 +0000

    working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def stripe_payment; end
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :friendships, only: [:create, :destroy]
   resources :users, only: [:show]
 
+  # root "application#stripe_payment"
+  resources :charges, only: :create
+  get :stripe_payment, to: 'application#stripe_payment'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
