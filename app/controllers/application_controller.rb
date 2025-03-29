4781f45ef852cd18e3ed0cacbbad74a26ee3cbe0
heidless <lockhart.r@gmail.com>
2025-03-29 12:55:03 +0000

    action text & Profile fomatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :bio, :avatar ])    # new
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name, :bio, :avatar ])   # edit
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name, :bio, :avatar, :content ])    # new
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name, :bio, :avatar, :content ])   # edit
   end
 end
